684d9f2baecfe150bd898893dea3910ab59dc2b5
Shakhzod <forworkingappsinmylaptop@gmail.com>
2021-12-26 20:54:59 +0500

    working sass file

---
+++ practice/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const options = {
+	name: 'test',
+	width: 1024,
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red'
+	}
+};
+console.log(options);
+delete options.name;
+console.log(options);
